b0d908d39314fe20efcbcf19c207df07b0457c91
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-05-18 17:42:26 +0200

    connecting the view to the code

--- PhoneBook/PhoneBook/ViewController.swift
+++ PhoneBook/PhoneBook/ViewController.swift
@@ -1,14 +1,12 @@
-//
-//  ViewController.swift
-//  PhoneBook
-//
-//  Created by Ghaiath Alhereh on 18.05.23.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contactsTableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
