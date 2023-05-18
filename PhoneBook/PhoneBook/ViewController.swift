a51cf515f274b507d9d2e7a803d0e6ee214180b4
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-05-18 17:24:00 +0200

    create a project

---
+++ PhoneBook/PhoneBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhoneBook
+//
+//  Created by Ghaiath Alhereh on 18.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
