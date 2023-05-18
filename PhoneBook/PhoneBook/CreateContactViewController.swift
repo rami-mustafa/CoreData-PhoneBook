80a2fb155f4b31292694c1b5642db4ad42c896f4
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-05-18 19:01:33 +0200

    Create Contact View Controller

---
+++ PhoneBook/PhoneBook/CreateContactViewController.swift
@@ -0,0 +1,28 @@
+
+import UIKit
+
+class CreateContactViewController: UIViewController {
+
+    @IBOutlet weak var personNameTextField: UITextField!
+    @IBOutlet weak var personNumberTextFiedl: UITextField!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func Add(_ sender: Any) {
+    }
+    
+}
