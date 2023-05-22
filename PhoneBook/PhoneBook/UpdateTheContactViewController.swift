a4d223f8b241859f1820d52b22bd0bc6d7377f8e
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-05-22 14:27:09 +0200

    data update process

--- PhoneBook/PhoneBook/UpdateTheContactViewController.swift
+++ PhoneBook/PhoneBook/UpdateTheContactViewController.swift
@@ -7,16 +7,30 @@ class UpdateTheContactViewController: UIViewController {
 
     @IBOutlet weak var personNameTextFiedl: UITextField!
     @IBOutlet weak var personNumberTextFiedl: UITextField!
-    
+
+    var Person:Contacts?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let p = Person {
+            personNameTextFiedl.text = p.person_name
+            personNumberTextFiedl.text = p.person_number
+        }
      }
     
 
  
     
     @IBAction func update(_ sender: Any) {
+        
+        if  let p = Person , let name =  personNameTextFiedl.text , let number = personNumberTextFiedl.text {
+           
+ 
+            p.person_name = name
+            p.person_number = number
+            
+            appDelegate.saveContext()
+        }
     }
     
 }
